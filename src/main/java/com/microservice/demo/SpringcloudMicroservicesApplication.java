268477b6dab9e5ca15e68e9414e3e417a578a964
lavanya nagumalli <pushkerkumar2@gmail.com>
2021-09-05 00:49:10 -0500

    Added request handler for requests using rest

--- src/main/java/com/microservice/demo/SpringcloudMicroservicesApplication.java
+++ src/main/java/com/microservice/demo/SpringcloudMicroservicesApplication.java
@@ -2,12 +2,22 @@ package com.microservice.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringcloudMicroservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringcloudMicroservicesApplication.class, args);
+	}
+	
+	@RequestMapping(value="/greeting", method=RequestMethod.GET)
+	public String sayHello()
+	{
+		return "Hello from Spring Boot";
 	}
 
 }
